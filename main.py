147f5ac5d47fa3c256af700ec2144076809f1d1a
lewis-github-1 <ltowne2@gmail.com>
2025-06-29 21:43:00 -0500

    added get_book_text() function and main()

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        print(f.read())
+
+
+
+
+def main():
+    get_book_text()
+
+
+
+
+if __name__ == "__main__":
+    main()
